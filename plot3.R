b0ab1b29d41aa38d2f608ab6a823e4305b9c8f1c
Ivero <Ivero@users.noreply.github.com>
2014-08-11 00:26:53 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,13 +1,19 @@
 ## Getting full dataset
-data_full <- read.csv("C:/Users/UVi/Desktop/exdata-data-household_power_consumption/household_power_consumption.txt", header=T, sep=';', na.strings="?",
+data_full <- read.csv(".../exdata-data-household_power_consumption/household_power_consumption.txt", header=T, sep=';', na.strings="?",
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+
 ## Subsetting the data
 data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(data_full)
+
+
 ## Converting dates
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
+
+
 ## Plot 3
 with(data, {
   plot(Sub_metering_1~Datetime, type="l",
@@ -17,6 +23,8 @@ with(data, {
 })
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+       
+       
 ## Saving to file
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
