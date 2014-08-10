48b0a2c4d14cda62e05c6a6061e6608abcbf29f4
Ivero <Ivero@users.noreply.github.com>
2014-08-11 00:27:49 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,13 +1,19 @@
 ## Getting full dataset
-data_full <- read.csv("C:/Users/UVi/Desktop/exdata-data-household_power_consumption/household_power_consumption.txt", header=T, sep=';', na.strings="?",
+data_full <- read.csv(".../exdata-data-household_power_consumption/household_power_consumption.txt", header=T, sep=';', na.strings="?",
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+
 ## Subsetting the data
 data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(data_full)
+
+
 ## Converting dates
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
+
+
 ## Plot 4
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(data, {
@@ -24,6 +30,8 @@ with(data, {
   plot(Global_reactive_power~Datetime, type="l",
        ylab="Global Rective Power (kilowatts)",xlab="")
 })
+
+
 ## Saving to file
 dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
